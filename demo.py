ab7d226d718e1782d5e0e8f74a7ac896d338657d
Louis Maddox <louismmx@gmail.com>
2024-07-17 11:34:32 +0100

    feat: use config field names as task entries

--- demo.py
+++ demo.py
@@ -1,19 +1,35 @@
 from pypdown import run_step
 from pypdown.models import Step
+from pydantic import BaseModel
+from pathlib import Path
+
+class StepParams(BaseModel):
+    n1_o: Path = "nil1.out"
+    n2_o: Path = "nil2.out"
+    a_i: Path = "a.in"
+    a_o: Path = "a.out"
+    b_o: Path = "b.out"
+    c_o: Path = "c.out"
+    d_i: Path = "d.in"
+    d_o: Path = "d.out"
+    e_i: Path = "e.in"
+    e_o: Path = "e.out"
+
+config = StepParams()
 
 file_tasks = [
-    ([], ["nil1.out"]),
-    (["a.in"], ["a.out"]),
-    (["a.out"], ["b.out"]),
-    (["a.out", "b.out"], ["c.out"]),
-    (["d.in"], ["d.out"]),
-    (["e.in"], ["e.out"]),
-    ([], ["nil2.out"]),
+    ([], ["n1_o"]),
+    (["a_i"], ["a_o"]),
+    (["a_o"], ["b_o"]),
+    (["a_o", "b_o"], ["c_o"]),
+    (["d_i"], ["d_o"]),
+    (["e_i"], ["e_o"]),
+    ([], ["n2_o"]),
 ]
 
-# Turn the in/output lists into dicts keyed by _-slugged filename
+# Turn the in/output lists into dicts keyed by config field name with filename values
 named_file_tasks = [
-    tuple({file.replace(".", "_"): file for file in files} for files in task)
+    tuple({field: getattr(config, field) for field in files} for files in task)
     for task in file_tasks
 ]
 
--- demo2.py
+++ demo2.py
@@ -1,14 +1,26 @@
 from pypdown import run_step
 from pypdown.models import Step
+from pydantic import BaseModel
+from pathlib import Path
+
+class StepParams(BaseModel):
+    a1_i: Path = "a1.in"
+    a2_i: Path = "a2.in"
+    a_o: Path = "a.out"
+    b_i: Path = "b.in"
+    b_o: Path = "b.out"
+
+config = StepParams()
+
 
 file_tasks = [
-    (["a1.in", "a2.in"], ["a.out"]),
-    (["a.out", "b.in"], ["b.out"]),
+    (["a1_i", "a2_i"], ["a_o"]),
+    (["a_o", "b_i"], ["b_o"]),
 ]
 
-# Turn the in/output lists into dicts keyed by _-slugged filename
+# Turn the in/output lists into dicts keyed by config field name with filename values
 named_file_tasks = [
-    tuple({file.replace(".", "_"): file for file in files} for files in task)
+    tuple({field: getattr(config, field) for field in files} for files in task)
     for task in file_tasks
 ]
 
