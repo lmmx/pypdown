7a7ee963c52c50b96732c13e839ae01391a094d2
Louis Maddox <lmmx@users.noreply.github.com>
2024-07-17 13:21:15 +0100

    feat: named task files (#2)
    * feat: named task files
    
    * chore(pre-commit): autofix run
    
    * feat: use config field names as task entries
    
    * feat: add callback function to task
    
    * feat: pass config (enable progressive introduction)
    
    * chore(pre-commit): autofix run
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/pypdown/models.py
+++ src/pypdown/models.py
@@ -1,6 +1,8 @@
 """Pydantic models to represent the tasks within a step in a data pipeline."""
 
 from pathlib import Path
+from typing import TypeVar
+from collections.abc import Callable
 
 from pydantic import BaseModel, FilePath, NewPath, OnErrorOmit, TypeAdapter
 
@@ -15,25 +17,34 @@ __all__ = [
 ]
 
 
-class AvailableTask(BaseModel):
+class Executable(BaseModel):
+    """All tasks must have an associated function to make them executable."""
+
+    fn: Callable
+
+
+class AvailableTask(Executable):
     """A task is available when its input files exist and its outputs don't."""
 
-    src: list[FilePath]
-    dst: list[NewPath]
+    src: dict[str, FilePath]
+    dst: dict[str, NewPath]
 
 
-class CompletedTask(BaseModel):
+class CompletedTask(Executable):
     """A task is completed when its output files exist, whether inputs exist or not."""
 
-    src: list[Path]
-    dst: list[FilePath]
+    src: dict[str, Path]
+    dst: dict[str, FilePath]
 
 
-class Task(BaseModel):
+class Task(Executable):
     """A task has zero or more input files and zero or more output files."""
 
-    src: list[Path]
-    dst: list[Path]
+    src: dict[str, Path]
+    dst: dict[str, Path]
+
+
+C = TypeVar("C", bound=BaseModel)
 
 
 class Step(BaseModel):
@@ -41,6 +52,7 @@ class Step(BaseModel):
 
     name: str
     tasks: list[Task]
+    config: C
 
 
 AvailableTA = TypeAdapter(list[OnErrorOmit[AvailableTask]])
